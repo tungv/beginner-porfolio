3259f7bd6a5650e7a8a7d1b2ff98f83f6da0d16b
Tung Vu <me@tungv.com>
2024-04-05 17:19:34 +0700

    first row of buttons are working
    Signed-off-by: Tung Vu <me@tungv.com>

--- calculator/src/app/page.tsx
+++ calculator/src/app/page.tsx
@@ -1,19 +1,48 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Home() {
+  const [currentValue, setCurrentValue] = useState("");
+
+  function addNumber(number: string) {
+    setCurrentValue(currentValue + number);
+  }
+
   return (
     <div>
       <h1>Calculator</h1>
 
       <div className="calculator">
         <div>
-          <input type="text" className="input" value="0" />
+          <input type="text" className="input" value={currentValue} />
         </div>
 
         <div className="button-panel">
-          <button className="button">7</button>
-          <button className="button">8</button>
-          <button className="button">9</button>
+          <button
+            className="button"
+            onClick={() => {
+              addNumber("7");
+            }}
+          >
+            7
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              addNumber("8");
+            }}
+          >
+            8
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              addNumber("9");
+            }}
+          >
+            9
+          </button>
 
           <button className="button">4</button>
           <button className="button">5</button>
